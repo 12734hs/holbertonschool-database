3b4a8e2a3c4154f7e6c64fd54617a91219d15993
SabSuf <12734@holbertonstudents.com>
2026-03-12 01:35:55 +0400

    Updated 0-uniq_users.sql

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  email CHAR(255) NOT NULL,
-  name CHAR(255),
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
   PRIMARY KEY(id),
   UNIQUE (email)
 );
