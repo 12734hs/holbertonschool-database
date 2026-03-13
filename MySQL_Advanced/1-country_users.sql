1e5544ef800521fd8991efe6a2d17118ffe7ec27
Sabir <12734@holbertonstudents.com>
2026-03-13 17:35:16 +0400

    FInished the task 1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,1 +1,8 @@
--- lol
+-- Create table with the id, email, name and country
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255) NOT NULL,
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
