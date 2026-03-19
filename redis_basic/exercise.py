ac2f780f6018bc87ef311148ac0ac996d141e1f7
Sabir <12734@holbertonstudents.com>
2026-03-19 13:42:56 +0400

    Have created the README  file and the first file of work

---
+++ redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+This is about asic redis file
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """This class we use for creating cache system"""
+
+    def __init__(self) -> None:
+        """
+        this method initialize the Cache system in the project where it were called
+        """
+        self._redis = redis.Redis(host = 'localhost', port=6379, decode_responses=True)
+        self._redis.flushdb()
+
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
