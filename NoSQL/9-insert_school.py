51b5120d7e2ef96287f4c279de8d297c4fa7efce
Sabir <12734@holbertonstudents.com>
2026-04-17 21:42:19 +0400

    Fixed nosql

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -8,6 +8,6 @@ def insert_school(mongo_collection, **kwargs):
     this is documentation, idk why we need it there
     so, this functions gets a collection, add the new data top the already existing collection, and returns the id
     """
-    data = mongo_collection.insert_one(**kwargs)
+    data = mongo_collection.insert_one(kwargs)
     return data.inserted_id
 
