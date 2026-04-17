5e52e92ad2ed74cdaa5489df0fe23bf379f10017
Sabir <12734@holbertonstudents.com>
2026-04-17 16:31:20 +0400

    FIxed some stuff

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,10 +2,7 @@
 """
 This is about asic redis file
 """
-try:
-    import redis
-except ImportError:
-    redis = None
+import redis
 import uuid
 from typing import Union, Callable, Optional, Any
 
@@ -28,25 +25,18 @@ class Cache:
         """
         This method store key-value pair in memory Redis
         """
-        if redis is not None:
-            key = str(uuid.uuid4())
-            self._redis.set(key, data)
-            return key
-        return 1
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
 
 
     def get(self, key: str, fn: Optional[Callable[[bytes], Any]] = None) -> Any:
         """
         This method returns the Value of the Key, and implement function if it was given
         """
-        if redis is not None:
-            value = self._redis.get(key)
-            if value is None:
-                return None
-            if fn:
-                return fn(value)
-            return value
-        return 1
+        value = self._redis.get(key)
+        return value
+
 
     def get_str(self, key: str) -> Optional[str]:
         """
